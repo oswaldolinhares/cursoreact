3d4852a4e90fd72d77c7f60135251b876eb8005c
Oswaldo Linhares <oswaldolinhares@gmail.com>
2018-02-13 12:39:13 -0300

    Aula 24 - Componentes Filhos

---
+++ exercicios_react/ex/silvaFamily.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+  <div>
+    <Member name = 'João' lastName = 'Linhares' />
+    <Member name = 'Maria' lastName = 'Linhares' />
+    <Member name = 'Jose' lastName = 'Linhares' />
+    <Member name = 'Pedro' lastName = 'Linhares' />
+  </div>
+)
