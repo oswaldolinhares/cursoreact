1a6d17bea848c0fb5139246d88eaf5aa83171eac
Oswaldo Linhares <oswaldolinhares@gmail.com>
2018-02-13 10:02:41 -0300

    Aula 15 - fazendo o css funcionar com o webpack

--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './ex3/index.js',
+  entry: './ex4/index.js',
   output: {
     path: __dirname + '/public',
     filename: './bundle.js'
@@ -10,6 +11,9 @@ module.exports = {
     port: 3000,
     contentBase: './public'
   },
+  plugins: [
+    new ExtractTextPlugin('app.css')
+  ],
   module: {
     loaders: [{
       test: /.js?$/,
@@ -19,6 +23,9 @@ module.exports = {
         presets: ['es2015', 'react'],
         plugins: ['transform-object-rest-spread']
       }
-    }]
+    }, {
+        test:/\.css/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+       }]
   }
 }
